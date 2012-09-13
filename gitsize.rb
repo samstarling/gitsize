73df78da684f42a77352c67ff787de4ca26648ed
Sam Starling <sam.starling@gmail.com>
2012-09-13 22:42:52 +0100

    Initial commit

---
+++ gitsize.rb
@@ -0,0 +1,20 @@
+require "github_api"
+require "sinatra"
+
+GITHUB = {
+  username: ENV['github_username',
+  password: ENV['github_password',
+  repo: 'samstarling.co.uk',
+  path: '_posts'
+}
+
+get '/' do
+  content_type 'text/plain'
+
+  github = Github.new login: GITHUB[:username], password: GITHUB[:password]
+  x = Array.new
+  github.repos.contents.get('samstarling', 'samstarling.co.uk', GITHUB[:path]).each do |file|
+    x << "#{file['size']} - #{file['name']}\n"
+  end
+  x
+end
